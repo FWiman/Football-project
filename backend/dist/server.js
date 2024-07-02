732c86572985998beb482c4df570d5abc7482309
Teodor Fredriksson <teo95@hotmail.se>
2024-07-02 16:46:54 +0200

    testing commit

--- backend/dist/server.js
+++ backend/dist/server.js
@@ -7,6 +7,11 @@ const dotenv_1 = __importDefault(require("dotenv"));
 dotenv_1.default.config();
 const app_1 = __importDefault(require("./app"));
 const port = process.env.PORT || 5000;
+const apiKey = process.env.API_FOOTBALL_TOKEN;
+if (!apiKey) {
+    console.error("API key not found. Please set API_FOOTBALL_TOKENAPI_FOOTBALL_TOKEN in your .env file.");
+    process.exit(1);
+}
 app_1.default.listen(port, () => {
     console.log(`Server is running on port ${port}`);
 });
