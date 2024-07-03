779473ecbb2ba0e0956c888969461dcda2c209ee
Teodor Fredriksson <teo95@hotmail.se>
2024-07-03 15:20:27 +0200

    updated post pre match news page with truncated text func

--- frontend/src/pages/PrePostMatchNews/PrePostMatchNewsPage.tsx
+++ frontend/src/pages/PrePostMatchNews/PrePostMatchNewsPage.tsx
@@ -10,9 +10,14 @@ const PrePostMatchNewsPage: React.FC = () => {
       <div>
         <h1 className={styles.sectionHeader}>Pre Game News</h1>
         <PrePostMatchSingleNewsComponent />
+        <PrePostMatchSingleNewsComponent />
+        <PrePostMatchSingleNewsComponent />
       </div>
       <div>
         <h1 className={styles.sectionHeader}>Post Game News</h1>
+        <PrePostMatchSingleNewsComponent />
+        <PrePostMatchSingleNewsComponent />
+        <PrePostMatchSingleNewsComponent />
         <PrePostMatchSingleNewsComponent />
       </div>
     </>
